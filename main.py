5dfe5f33d1eae301f2e510beb3f74fbb0c324e68
Ryan Talbert <ryan.talbert@turnberrysolutions.com>
2025-07-01 11:46:26 -0500

    cleans up instructions

--- main.py
+++ main.py
@@ -8,21 +8,17 @@ import os
 
 async def main():
     params = {
-  "mcpServers": {
-    "github": {
-      "command": "docker",
-      "args": [
-        "run",
-        "-i",
-        "--rm",
-        "-e",
-        "GITHUB_PERSONAL_ACCESS_TOKEN",
-        "mcp/github"
-      ],
-      "env": {
-        "GITHUB_PERSONAL_ACCESS_TOKEN": os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
-      }
-    }
+  "command": "docker",
+  "args": [
+    "run",
+    "-i",
+    "--rm",
+    "-e",
+    "GITHUB_PERSONAL_ACCESS_TOKEN",
+    "mcp/github"
+  ],
+  "env": {
+    "GITHUB_PERSONAL_ACCESS_TOKEN": os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
   }
 }
 
@@ -30,22 +26,25 @@ async def main():
 
     instructions = """
 You are a Github expert who's goal is to make sure every PR that is created has a proper description.
-You will use your tools to find a PR, analyze the code, and make sure the PR description is expertly written.
+You will use your tools to find an open PR, analyze the code, and make sure the PR description is expertly written.
 You will balance ease of reading with technical accuracy.
+Please be sure to explain what the end goal of the code in the PR is actually doing.
+If there is no PR description, edit the original empty description with your own description.
 Thank you for your help!
 """
 
-    async with MCPServerStdio(params=params, client_session_timeout_seconds=150) as mcp_server:
-        print(mcp_server.list_tools())
+    async with MCPServerStdio(params=params, client_session_timeout_seconds=150) as server:
+      
         agent = Agent(
             name="Github Expert",
             instructions=instructions,
-            tools=[mcp_server],
+            mcp_servers=[server],
+            model="gpt-4o-mini",
         )
-        
-        await Runner.run(
-            agent, "Please find the github-mcp repo, find the latest PR, and make sure the description is properly written. Thank you!"
-        )
+        with trace("Github Expert"):
+            await Runner.run(
+                agent, "Please find the ryanturnberry/github-mcp repo, find the latest PR, and make sure the description is properly written. Thank you!"
+            )
 
 
 if __name__ == "__main__":
